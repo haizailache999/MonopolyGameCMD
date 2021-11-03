234162c625c621541bfc3364589daeca310705be
haizailache999 <90511158+haizailache999@users.noreply.github.com>
2021-11-03 20:32:09 +0800

    Update ChanceSquareTest.java

--- src/test/java/hk/edu/polyu/comp3211/g27/model/square/ChanceSquareTest.java
+++ src/test/java/hk/edu/polyu/comp3211/g27/model/square/ChanceSquareTest.java
@@ -4,9 +4,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ChanceSquareTest extends SquareTestBase {
+    private int[] SquareIndex={9,13,19};
+    private String SquareLabel= "CHANCE";
     @Test
     @DisplayName("Should affect current player's money between -300 and 200")
     public void shouldAffectMoney() {
-
+        for (int i=0;i<2;i++){
+            ChanceSquare chanceSquare =new ChanceSquare(SquareIndex[i],SquareLabel);
+            verify(chanceSquare).onEnter(game);
+        }
     }
 }
