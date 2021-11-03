50e48cbd9d9578565a747e07cc430fb112975c1f
haizailache999 <90511158+haizailache999@users.noreply.github.com>
2021-11-03 20:27:46 +0800

    Update GoToJailSquareTest.java

--- src/test/java/hk/edu/polyu/comp3211/g27/model/square/GoToJailSquareTest.java
+++ src/test/java/hk/edu/polyu/comp3211/g27/model/square/GoToJailSquareTest.java
@@ -4,9 +4,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class GoToJailSquareTest extends SquareTestBase {
+    private int squareIndex=16;
+    private String squareLabel = "GO TO JAIL";
     @Test
     @DisplayName("Should put the current player in jail")
     public void shouldPutInJail() {
-
+        GoToJailSquare goToJailSquare=new GoToJailSquare(squareIndex,squareLabel);
+        verify(goToJailSquare).onEnter(game);
     }
 }
