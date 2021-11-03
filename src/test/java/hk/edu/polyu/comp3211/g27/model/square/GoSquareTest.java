e11da1a575766eae93883b04d15d220779acec25
haizailache999 <90511158+haizailache999@users.noreply.github.com>
2021-11-03 20:28:55 +0800

    Update GoSquareTest.java

--- src/test/java/hk/edu/polyu/comp3211/g27/model/square/GoSquareTest.java
+++ src/test/java/hk/edu/polyu/comp3211/g27/model/square/GoSquareTest.java
@@ -5,9 +5,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class GoSquareTest extends SquareTestBase {
+    private int SquareIndex = 1;
+    private String SquareLabel = "GO";
     @Test
     @DisplayName("Should supply the current player with 1500 HKD")
     public void shouldSupplyMoney() {
-
+        GoSquare goSquare = new GoSquare(SquareIndex,SquareLabel);
+        verify(goSquare).onEnter(game);
     }
 }
